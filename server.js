40174d6989dbe6b103afc94866d5603479a6d6ec
Aditya Kashyap <Kashyapaladitya07@gmail.com>
2025-07-08 11:13:39 +0530

    Update server.js with new changes

--- server.js
+++ server.js
@@ -5,12 +5,18 @@ const cors = require('cors'); // CORS module included
 const authRoutes = require('./src/routes/authRoutes'); // Importing auth routes
 const linksRoutes = require('./src/routes/linksRoutes'); // Importing links routes
 const userRoutes = require('./src/routes/userRoutes');
-
+const paymentRoutes = require('./src/routes/paymentRoutes')
  require('dotenv').config(); // Load environment variables from .env file
 
  const mongoose = require('mongoose'); 
 const app = express(); // instances of express application 
-app.use(express.json()); // Middleware to parse JSON bodies
+app.use((request,response,next) => {
+    if(request.originalUrl.startsWith('/payments/webhook')){
+        return next();
+    }
+
+    express.json()(request,response,next);
+})
 app.use(cookieParser()); // Middleware to parse cookies
 
 const corsOptions = {
@@ -23,6 +29,7 @@ app.use(cors(corsOptions)); // Use CORS middleware with specified options
 app.use('/auth', authRoutes); // Mounting auth routes on /auth path
 app.use('/links', linksRoutes); // Mounting links routes on /links path\
 app.use('/users',userRoutes);
+app.use('/payments',paymentRoutes);
 
 mongoose.connect(process.env.MONGO_URI).then(() => console.log('Database connected successfully'))
 .catch((error) =>
